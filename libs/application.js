f438eee3b3c1430dbe74f39dcff77c8a95f1d9cd
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2018-06-03 18:36:11 +0800

    mkdir

--- libs/application.js
+++ libs/application.js
@@ -1,16 +1,21 @@
 
 const Emitter = require('events');
-
+const fse = require('fs-extra');
+const path = require('path');
+const cwd = process.cwd();
 
 module.exports = class Application extends Emitter {
-  constructor({ pname,  template}){
+  constructor({ pname,  template, fullPath, dest}){
     super();
     this.pname = pname;
     this.template = template;
+    this.fullPath = fullPath;
+    this.dest = path.join(cwd, this.pname);
   }
 
   async create(){
-    console.log('create');
+    await fse.ensureDir(this.dest);
+    // console.log('create', this.pname, this.template, this.fullPath, cwd);
     return this;
   }
 }
