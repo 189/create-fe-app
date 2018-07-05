0a0480d8725783d7f2d2065e7f749f249300607a
王尚文 <wangshangwen@wangshangwendeMacBook-Pro.local>
2018-07-05 21:09:55 +0800

    update

---
+++ libs/restore.js
@@ -0,0 +1,21 @@
+
+
+// npm run restore -- --dir dirname --...
+
+const fs = require('fs-extra');
+const parseArgs = require('minimist')(process.argv.slice(2));
+const dir = parseArgs['dir'];
+const path = require('path');
+
+if(dir){
+  const dirPath = path.resolve(__dirname, '../templates', dir);
+  const exist = fs.existsSync(dirPath);
+  if(!exist){
+    console.error(dir + 'is not exist : [' + dirPath + ']');
+    return process.exit(0);
+  }
+  const pkg = path.resolve(dirPath, 'package.json');
+  const _pkg = path.resolve(dirPath, '_package.json');
+  fs.renameSync(pkg, _pkg);
+}
+
