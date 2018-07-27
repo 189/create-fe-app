2c05265764f7d8439cc103e41fcd1f8039210e03
kelvinwang <kv.wang87@gmail.com>
2018-07-27 18:27:40 +0800

    inject store

--- templates/react-spa2/src/app.js
+++ templates/react-spa2/src/app.js
@@ -3,28 +3,38 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import "./assets/styles/style.scss";
+import configurestore from './pub/store/configureStore';
+import "./assets/styles/index.scss";
 import Home from './pages/Home';
 import About from './pages/About';
 import Topics from './pages/Topics';
 
-const App = () => {
-    return (
-        <Router>
+
+const store = new configurestore();
+
+class App extends React.Component {
+    render(){
+        return (
             <div>
-                <ul>
-                    <li> <Link to="/">Home1</Link> </li>
-                    <li> <Link to="/about">About</Link> </li>
-                    <li> <Link to="/topics">Topics</Link> </li>
-                </ul>
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/topics" component={Topics} />
+                当前用户：<span>fx</span>
+                <Router>
+                    <div>
+                        <ul>
+                            <li> <Link to="/">Home1</Link> </li>
+                            <li> <Link to="/about">About</Link> </li>
+                            <li> <Link to="/topics">Topics</Link> </li>
+                        </ul>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/about" component={About} />
+                        <Route path="/topics" component={Topics} />
+                    </div>
+                </Router>
             </div>
-        </Router>
-    );
+        );
+    }
 };
 
+console.log(store);
 const node = document.querySelector('#wrap');
 ReactDOM.render(<App />, node);
 
