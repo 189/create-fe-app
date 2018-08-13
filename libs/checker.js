0dd71174673a5e1da296180d5b5558c2b5b3f71a
189 <>
2018-08-13 17:34:46 +0800

    bugfix

--- libs/checker.js
+++ libs/checker.js
@@ -4,8 +4,12 @@ const {versions, exit} = process;
 
 module.exports = {
   version(){
+    // console.log(parseFloat(versions.node));
+    // exit(0);
     if(parseFloat(versions.node) < 8.6){
       console.error(`
+        Version is ${versions.node} \n
+        Your current version is ${parseFloat(versions.node)}\n
         The cli require node version ≥ v8.6 for object rest/spread property  \n
         Please update your node version
       `
